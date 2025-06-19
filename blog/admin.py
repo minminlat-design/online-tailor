2a1477bbe05f95bb9845801c57f0d4e3a56bd617
minminlat-design <minminlat.shirt@gmail.com>
2025-06-19 14:26:32 +0630

    after finish half blog setup

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django import forms
+from ckeditor.widgets import CKEditorWidget
+from .models import Post, PostImage       
+from easy_thumbnails.files import get_thumbnailer
+from django.utils.html import format_html
 
-# Register your models here.
+
+
+class PostAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+        
+
+
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    extra = 3
+    fields = ('order', 'image', 'thumbnail_preview', 'caption')  # add thumbnail_preview here
+    readonly_fields = ('thumbnail_preview',)  # make it readonly so it's not editable
+    ordering = ('order',)
+
+    def thumbnail_preview(self, obj):
+        if obj.image:
+            thumbnail_url = get_thumbnailer(obj.image).get_thumbnail({'size': (100, 100), 'crop': True}).url
+            return format_html('<img src="{}" width="100" height="100" style="object-fit: cover;" />', thumbnail_url)
+        return "(No image)"
+
+    thumbnail_preview.short_description = 'Preview'
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+    list_display = ('title', 'slug', 'author', 'status', 'publish', 'created')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish',)
+    inlines = [PostImageInline]
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,70 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+from ckeditor.fields import RichTextField
+from django.urls import reverse
 
-# Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='PB')
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True, unique_for_date='publish')
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts',
+        default=1
+    )
+    description = RichTextField('Description', config_name='default', blank=True, null=True)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
+    
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail', 
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+                ]
+            )
+
+
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='blog_images/%Y/%m/%d/')
+    caption = models.CharField(max_length=255, blank=True)  # optional
+    order = models.PositiveIntegerField(default=0, blank=False, null=False)
+
+    class Meta:
+        ordering = ['order']  # ensures images come out ordered by this field
+
+    def __str__(self):
+        return f"Image for post: {self.post.title} (Order: {self.order})"
